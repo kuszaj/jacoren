c20293783432b1cdb4fd873f1d64f2651b219a24
Piotr Kuszaj <peterkuszaj@gmail.com>
2017-05-29 19:22:53 +0200

    Redefine jacoren.platform.VERSION

--- jacoren/platform.py
+++ jacoren/platform.py
@@ -4,10 +4,4 @@ import platform
 
 
 #: Platform version tuple (major, minor, release)
-VERSION = platform.release().split('.')
-if len(VERSION) > 3:
-    VERSION = (VERSION[0],  # major
-               VERSION[1],  # minor
-               VERSION[2:]) # release
-else:
-    VERSION = tuple(VERSION)
+VERSION = tuple( platform.release().split('.', 2) )
