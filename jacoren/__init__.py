ed173563f7e65d6d851ead96fc71c2390b156cc7
Piotr Kuszaj <peterkuszaj@gmail.com>
2017-05-29 19:03:07 +0200

    Added PLATFORM_RELEASE

--- jacoren/__init__.py
+++ jacoren/__init__.py
@@ -1,8 +1,28 @@
 # -*- coding: utf-8 -*-
 
+import platform
+import psutil
+
+
+#:
+#: Package info
+#:
 __version__ = '0.0.1'
 __title__ = 'jacoren'
 __description__ = ''
 __author__ = 'Piotr Kuszaj'
 __author_email__ = 'peterkuszaj@gmail.com'
 __license__ = 'MIT'
+
+#:
+#: Platform
+#:
+
+#: Platform version tuple (major, minor, release)
+PLATFORM_VERSION = platform.release().split('.')
+if len(PLATFORM_VERSION) > 3:
+    PLATFORM_VERSION = (PLATFORM_VERSION[0],  # major
+                        PLATFORM_VERSION[1],  # minor
+                        PLATFORM_VERSION[2:]) # release
+else:
+    PLATFORM_VERSION = tuple(PLATFORM_VERSION)
