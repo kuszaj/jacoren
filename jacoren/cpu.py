d8842ec8f73f85566033dd402c6c0313589bb32d
Piotr Kuszaj <peterkuszaj@gmail.com>
2017-05-29 19:14:28 +0200

    Added cpu.cpu_load()

--- jacoren/cpu.py
+++ jacoren/cpu.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import psutil
+from jacoren.platform import VERSION as _platform_version
 
 
 #: Core count (physical and logical)
@@ -13,3 +14,37 @@ def cpu_info():
         'cores': CORES,
         'physical_cores': PHYSICAL_CORES
     }
+
+def cpu_load(cpu_time=False):
+    #: Platform-independent
+    fields = ('user', 'system', 'idle')
+
+    #: Platform-specific
+    if psutil.LINUX:
+        if _platform_version >= ('3', '2', '0'):
+            _fields = _fields + (
+                'nice', 'iowait', 'irq', 'softirq',
+                'steal', 'guest', 'guest_nice',
+            )
+        elif _platform_version >= ('2', '6', '24'):
+            _fields = _fields + (
+                'nice', 'iowait', 'irq', 'softirq',
+                'steal', 'guest',
+            )
+        elif _platform_version >= ('2', '6', '11'):
+            _fields = _fields + (
+                'nice', 'iowait', 'irq', 'softirq',
+                'steal',
+            )
+        else:
+            _fields = _fields + (
+                'nice', 'iowait', 'irq', 'softirq',
+            )
+    elif psutil.BSD:
+        _fields = _fields + ('nice', 'irq')
+    elif psutil.POSIX:
+        _fields = _fields + ('nice')
+    elif psutil.WINDOWS:
+        _fields = _fields + ('interrupt', 'dpc')
+    else:
+        raise RuntimeError("cpuinfo not supported")
