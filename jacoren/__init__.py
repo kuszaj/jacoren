19d916bc35dee582276b320f02e2c3d18b76bfe5
Piotr Kuszaj <peterkuszaj@gmail.com>
2017-05-29 18:56:01 +0200

    Initial commit

---
+++ jacoren/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.0.1'
+__title__ = 'jacoren'
+__description__ = ''
+__author__ = 'Piotr Kuszaj'
+__author_email__ = 'peterkuszaj@gmail.com'
+__license__ = 'MIT'
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from jacoren import (
+    __version__ as version,
+    __title__ as title,
+    __description__ as description,
+    __author__ as author,
+    __author_email__ as author_email,
+    __license__ as license,
+)
+
+# Requirements
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name=title,
+    version=version,
+    description=description,
+    author=author,
+    author_email=author_email,
+    license=license,
+    packages=['jacoren'],
+    package_dir={'jacoren': 'jacoren'},
+    install_requires=requirements,
+)
