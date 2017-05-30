655eb03980a23d00e4910517264ccd9305e19312
Piotr Kuszaj <peterkuszaj@gmail.com>
2017-05-30 10:45:49 +0200

    Added jacoren.platform.OS

--- jacoren/platform.py
+++ jacoren/platform.py
@@ -5,5 +5,15 @@
 import platform
 
 
+#: Platform OS
+OS = platform.system()
+
 #: Platform version tuple (major, minor, release)
 VERSION = tuple(platform.release().split('.', 3))
+
+
+def platform():
+    return {
+        'os': OS,
+        'version': VERSION,
+    }
