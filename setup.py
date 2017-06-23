db9506063638a3a45a468a8b1a0d8330ed51eeac
Piotr Kuszaj <peterkuszaj@gmail.com>
2017-06-23 17:59:13 +0200

    Preparation for tests

--- setup.py
+++ setup.py
@@ -12,6 +12,9 @@ from jacoren import (
 with open('requirements.txt', 'r') as f:
     requirements = f.read().splitlines()
 
+tests_requirements = ['pytest>=3.1.0', 'psutil>=5.2.0']
+setup_requirements = ['pytest-runner']
+
 
 setup(
     name=title,
@@ -23,6 +26,8 @@ setup(
     packages=['jacoren'],
     package_dir={'jacoren': 'jacoren'},
     install_requires=requirements,
+    setup_requires=setup_requirements,
+    tests_require=tests_requirements,
     entry_points = {
         'console_scripts': ['jacoren=jacoren._server:main'],
     }
